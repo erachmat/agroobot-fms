1774c50989b9537847666890b3ab2414d46607a8
erachmat <15140029+erachmat@users.noreply.github.com>
2022-12-31 11:42:21 +0700

    add ApiClient and GetService initial setup

--- app/src/main/java/com/example/agroobot_fms/api/ApiClient.java
+++ app/src/main/java/com/example/agroobot_fms/api/ApiClient.java
@@ -4,11 +4,12 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
+
     public static Retrofit retrofit;
     public static final String BASE_URL = "https://jrvis-frday.id";
 
-    public static Retrofit getRetrofitInstance(){
-        if (retrofit == null){
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/example/agroobot_fms/api/GetService.java
+++ app/src/main/java/com/example/agroobot_fms/api/GetService.java
@@ -1,5 +1,8 @@
 package com.example.agroobot_fms.api;
 
+import com.example.agroobot_fms.model.get_all_data_panen.DataPanen;
+import com.example.agroobot_fms.model.get_one.GetOne;
+import com.example.agroobot_fms.model.get_one.GetOneBody;
 import com.example.agroobot_fms.model.list_menu.ListMenu;
 import com.example.agroobot_fms.model.list_menu.ListMenuBody;
 import com.example.agroobot_fms.model.login.LoginBody;
@@ -7,15 +10,24 @@ import com.example.agroobot_fms.model.login.LoginResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface GetService {
 
     @POST("/api/login")
     Call<LoginResponse> loginAccount(@Body LoginBody loginBody);
 
-    @POST("api/login/menu")
+    @POST("/api/login/menu")
     Call<ListMenu> listMenu(@Body ListMenuBody loginBody);
+
+    @GET("/api/datapanen/get-all")
+    Call<DataPanen> getAllDataPanen();
+
+
+    @GET("/api/datapanen/get-one")
+    Call<DataPanen> getOneDataPanen(@Query("id_seq") int idSeq);
 
 
 }
