ad8a71978eb913a0f0a4a2e0822ea3c96aa7302b
erachmat <15140029+erachmat@users.noreply.github.com>
2023-01-14 12:54:39 +0700

    add JadwalFragment layout

--- app/src/main/java/com/example/agroobot_fms/JadwalFragment.java
+++ app/src/main/java/com/example/agroobot_fms/JadwalFragment.java
@@ -7,13 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link JadwalFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.chivorn.smartmaterialspinner.SmartMaterialSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JadwalFragment extends Fragment {
+
+    private SmartMaterialSpinner<String> spProvince;
+    private SmartMaterialSpinner<String> spEmptyItem;
+    private List<String> provinceList;
 
     public JadwalFragment() {
         // Required empty public constructor
@@ -34,7 +40,34 @@ public class JadwalFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_jadwal, container, false);
 
+        initSpinner(view);
 
         return view;
+    }
+
+    private void initSpinner(View view) {
+        spProvince = view.findViewById(R.id.sp_petani);
+//        spEmptyItem = view.findViewById(R.id.sp_empty_item);
+        provinceList = new ArrayList<>();
+
+        provinceList.add("Kampong Thom");
+        provinceList.add("Kampong Cham");
+        provinceList.add("Kampong Chhnang");
+        provinceList.add("Phnom Penh");
+        provinceList.add("Kandal");
+        provinceList.add("Kampot");
+
+        spProvince.setItem(provinceList);
+
+        spProvince.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getActivity(), provinceList.get(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
 }
